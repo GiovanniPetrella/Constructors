7a962392cb871b542c509e74635936a829c90d78
GiovanniPetrella <tangolikeamango1@gmail.com>
2015-08-27 19:21:04 -0700

    Constructors
    Constructors

---
+++ src/apples.java
@@ -0,0 +1,8 @@
+class apples {
+	public static void main(String args[]){
+		tuna tunaObject = new tuna("Kelsey");
+		tuna tunaObject2 = new tuna("Nicole");
+		tunaObject.saying();
+		tunaObject2.saying();
+	}
+}
